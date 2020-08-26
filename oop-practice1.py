92cbece594a7263faa4cc72fc71922c1b8955e32
Katherine Frain <katherfrain@gmail.com>
2020-08-26 12:37:33 -0400

    making sure

--- oop-practice1.py
+++ oop-practice1.py
@@ -4,10 +4,12 @@ class Person:
          self.email = email
          self.phone = phone
          self.friends = []
-
+         self.greetingcount = 0
 
     def greet(self, other_person):
-         print('Hello {}, I am {}!'.format(other_person.name, self.name)) 
+         print('Hello {}, I am {}!'.format(other_person.name, self.name))
+         self.greetingcount = self.greetingcount + 1
+         return self.greetingcount
     
     def print_contact_info(self):
         print(f"{self.name}'s email: {self.email}\n{self.name}'s phone number: {self.phone}.")
@@ -16,6 +18,12 @@ class Person:
         self.friends.append(other)
         for friend in self.friends:
             print(friend.name)
+    
+    def numfriends(self):
+        return len(self.friends)
+
+    def greeting_count(self):
+        return self.greetingcount
     
 sonny = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
 jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
@@ -30,3 +38,5 @@ sonny.print_contact_info()
 
 jordan.add_friend(sonny)
 jordan.add_friend(mark)
+print(jordan.numfriends())
+print(jordan.greeting_count())
